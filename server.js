9338b4bce753c93d89df3faa941c6851726e6338
RU XING XIAO <xiaorux@cn.ibm.com>
2020-04-16 17:16:30 +0800

    add config

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var app = express();
+var proxy = require('http-proxy-middleware');
 
 // Listen
-
-var proxy = require('express-http-proxy');
-
-app.use(express.static('public'));
-
-app.use('/api', proxy('http://ibmcloudappmgmt-ibm-cem-cem-users.kube-system.svc:6002'))
+app.use('/api', proxy.createProxyMiddleware(
+    { target: 'http://ibmcloudappmgmt-ibm-cem-cem-users.kube-system.svc:6002',
+ changeOrigin: true }));
 
 app.listen(8080);
 console.log('Listening on localhost:'+ 8080);
