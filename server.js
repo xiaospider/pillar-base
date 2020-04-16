972ba8adb10909d5d798538f4051180f8d2e823a
RU XING XIAO <xiaorux@cn.ibm.com>
2020-04-16 16:43:21 +0800

    update port

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ app.use(express.static('public'));
 
 app.use('/api', proxy('http://ibmcloudappmgmt-ibm-cem-cem-users.kube-system.svc:6002'))
 
-app.listen(3001);
-console.log('Listening on localhost:'+ 3001);
+app.listen(8080);
+console.log('Listening on localhost:'+ 8080);
